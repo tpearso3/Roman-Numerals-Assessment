415dfc75d57d06fe1f98d15f6b6159c9cebed586
Tanner <pearsontanner345@gmail.com>
2020-01-02 15:13:16 -0700

    Committing Roman Numerals Assessment, assumptions and required software found in README

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-debug = False
+debug = True
 
 #function to put guesses into the debugger
 def get_next_roman_num():
   next_roman_num_list = ["I", "II", "III", "IV", "V",
                         "VI", "VII", "VIII", "IX", "X",
-                     "a", "A", "apple", "1", "2",
-                     "XM", "CX", "CLXV", "XL",
-                     "XC", "XCIV"]
+                         "XI", "XII", "XIII", "XIV", "XV",
+                         "XVI", "XVII", "XVIII", "XIX", "XX",
+                         "XXI", "XXII", "XXIII", "XXIV", "XXV",
+                         "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+                         "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV",
+                         "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                         "XLI", "XLII", "XLIII", "XLIV", "XLV",
+                         "XLVI", "XLVII", "XLVIII", "XLIX", "L"]
   return next_roman_num_list
+
+#function to get numbers to test roman nums
+def get_next_expected_num():
+  next_expected_num_list = [1, 2, 3, 4, 5,
+                            6, 7, 8, 9, 10,
+                            11, 12, 13, 14, 15,
+                            16, 17, 18, 19, 20,
+                            21, 22, 23, 24, 25,
+                            26, 27, 28, 29, 30,
+                            31, 32, 33, 34, 35,
+                            36, 37, 38, 39, 40,
+                            41, 42, 43, 44, 45,
+                            46, 47, 48, 49, 50]
+  return next_expected_num_list
 
 #function to check if roman numerals are entered correctly
 def check_for_roman_numerals(roman_num):
@@ -31,6 +50,36 @@ def check_for_roman_numerals(roman_num):
     else:
       is_roman_num = False
   return is_roman_num
+
+#function to make indexes out of the roman num
+def index_roman_num(roman_num):
+  index = list(roman_num)
+  for i in range(len(index)):
+    if index[i] == "I":
+      index[i] = 1
+    if index[i] == "V":
+      index[i] = 2
+    if index[i] == "X":
+      index[i] = 3
+    if index[i] == "L":
+      index[i] = 4
+    if index[i] == "C":
+      index[i] = 5
+    if index[i] == "D":
+      index[i] = 6
+    if index[i] == "M":
+      index[i] = 7
+  return index
+
+#function to check
+def check_for_rule_of_two(index):
+  rule_of_two = True
+  for i in range(len(index) - 1):
+    if (index[i + 1] - index[i]) <= 2:
+      continue
+    else:
+      rule_of_two = False
+  return rule_of_two
 
 #function to convert roman numerals to numbers
 def convert_roman_num_to_num(roman_num):
@@ -84,26 +133,29 @@ def subtract_num(A, B):
 
 #function to find if A < B, and create a new list with new values
 def subtract_and_simplify(num_list):
-  new_num_list = list()
-  for i in range(len(num_list) - 1):
-    if num_list[i] < num_list[i + 1]: #if A < B
-      new_num_list.append(subtract_num(num_list[i], num_list[i + 1]))
-      #add subtracted total to new_num_list
-    if num_list[i] == num_list[i + 1]: #if A = B
-      new_num_list.append(num_list[i])  #add A to new_num_list
-      if (i + 1) == (len(num_list) - 1):  #if B is the last num in num_list
-        new_num_list.append(num_list[i + 1])  #add B to new_num_list
-    if num_list[i] > num_list[i + 1]: #if A > B
-      if i == 0:  #if A is the first num in num_list
-        new_num_list.append(num_list[i])  #add A to new_num_list
-        if (i + 1) == (len(num_list) - 1): #if B is the last num in num_list
-          new_num_list.append(num_list[i + 1])  #add B to new_num_list
-      if num_list[i] > num_list[i - 1]: #if A > num before A
-        continue  #don't add A
-      else: #otherwise
-        new_num_list.append(num_list[i])  #add A to new_num_list
-        if (i + 1) == (len(num_list) - 1):  #if B is last num in num_list
-          new_num_list.append(num_list[i + 1])  #add B to new_num_list
+  new_num_list = list() #make new list to hold new values
+  size_num_list = len(num_list)
+  for i in range(size_num_list):
+    if i == 0: #A is the first num in list
+      if num_list[i] < num_list[i + 1]: #A < B
+        new_value = subtract_num(num_list[i], num_list[i + 1]) #C = B - A
+        new_num_list.append(new_value) #add C
+      if num_list[i] == num_list[i + 1] or num_list[i] > num_list[i + 1]: #A = B or A > B
+        new_num_list.append(num_list[i]) #add A
+    if i == (size_num_list - 1): #A is the last num in list
+      if num_list[i] > num_list[i - 1]: #A > num before
+        continue
+      if num_list[i] == num_list[i - 1] or num_list[i] < num_list[i - 1]: #A = num bef or A < num bef
+        new_num_list.append(num_list[i]) #add A
+    if i != 0 and i != (size_num_list - 1): #A is in the middle of the list
+      if num_list[i] > num_list[i - 1]: #A > num before
+        continue
+      else:
+        if num_list[i] < num_list[i + 1]: #A < B
+          new_value = subtract_num(num_list[i], num_list[i + 1]) #C = B - A
+          new_num_list.append(new_value) #add C
+        if num_list[i] == num_list[i + 1] or num_list[i] > num_list[i + 1]: #A = B or A > B
+          new_num_list.append(num_list[i]) #add A
   return new_num_list
 
 #function to total up the numbers
@@ -118,21 +170,34 @@ def main():
     roman_num_list_length = len(get_next_roman_num())
     for i in range(roman_num_list_length):
       roman_num = get_next_roman_num()[i]
+      expected_num = get_next_expected_num()[i]
       if (check_for_roman_numerals(roman_num)): #if roman num are entered correctly
         num_list = convert_roman_num_to_num(roman_num) #convert roman num to nums
         if (len(num_list)) == 1: #if there is only one num
           number = add_new_list(num_list) #convert num from list to num
-          print(roman_num, " = ", number)
-        else: #if there is more than one num
-          if check_for_tens(num_list):  #if nums follow tens rule
-            new_num_list = subtract_and_simplify(num_list)
-            #check for sub and make new list
-            number = add_new_list(new_num_list) #add up new list to get num
-            print(roman_num, " = ", number)
+          if number == expected_num:
+            continue
           else:
-            print(roman_num, " Is an Invalid Roman Numeral")
+            print("wrong calc ", roman_num, " expected ",
+                  expected_num, " got ", number)
+        else: #if there is more than one num
+          index = index_roman_num(roman_num)
+          if check_for_rule_of_two(index):
+            if check_for_tens(num_list):  #if nums follow tens rule
+              new_num_list = subtract_and_simplify(num_list)
+              #check for sub and make new list
+              number = add_new_list(new_num_list) #add up new list to get num
+              if number == expected_num: #check if calculating correct num
+                continue
+              else: #print out message if calculation is incorrect
+                print("wrong calc, ", roman_num, " expected ",
+                      expected_num, " got ", number)
+            else:
+              print(roman_num, " is an invalid roman numeral")
+          else:
+            print(roman_num, " is an invalid roman numeral")
       else:
-        print(roman_num, " Is an Invalid Roman Numeral")
+        print(roman_num, " is an invalid roman numeral")
   else: #if debug mode not activated
     roman_num = input("Enter Roman Numeral: ") #user inputs, roman num
     if (check_for_roman_numerals(roman_num)): #if roman num are entered correctly
@@ -141,15 +206,19 @@ def main():
         number = add_new_list(num_list) #convert num from list to num
         print(roman_num, " = ", number)
       else: #if there is more than one num
-        if check_for_tens(num_list):  #if nums follow tens rule
-          new_num_list = subtract_and_simplify(num_list)
-          #check for sub and make new list
-          number = add_new_list(new_num_list) #add up new list to get num
-          print(roman_num, " = ", number)
-        else:
-          print(roman_num, " Is an Invalid Roman Numeral")
+          index = index_roman_num(roman_num)
+          if check_for_rule_of_two(index):
+            if check_for_tens(num_list):  #if nums follow tens rule
+              new_num_list = subtract_and_simplify(num_list)
+              #check for sub and make new list
+              number = add_new_list(new_num_list) #add up new list to get num
+              print(roman_num, " = ", number)
+            else:
+              print(roman_num, " is an invalid roman numeral")
+          else:
+            print(roman_num, " is an invalid roman numeral")
     else:
-      print(roman_num, " Is an Invalid Roman Numeral")
+      print(roman_num, " is an invalid roman numeral")
 
 
 if __name__ == "__main__":
